05e2563a0649300f939b5cbd42dbde7d0a67c46a
atroman <roman.attinger@env.ethz.ch>
2021-03-22 10:56:26 +0100

    added optional argument to specify NA values

--- R/rfile.R
+++ R/rfile.R
@@ -2,15 +2,17 @@
 #'
 #' @param file character string specifying the NetCDF file location
 #' @param field character string specifying the field to be read
-#' @param ... arbitrary number of additional character strings specifying additional fields to be read
+#' @param navals float specifying NA values
+#' @param ... arbitrary number of character strings specifying additional fields to be read
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' readfile("/net/server/folder/somefile.nc","SLP","OMEGA")
+#' library(zeallot) # Need this to unpack data from rfile
+#' c(APVTURBT)%<-%rfile("/net/thermo/atmosdyn2/atroman/stingjet/20140211_12/tra/APV12_20140212_07.nc","APVTURBT")
 
-rfile <-function(file,field,...) {
+rfile <-function(file,field,...,navals=-999) {
 
   # Check if file exists
   if(!file.exists(file)) {
@@ -21,12 +23,16 @@ rfile <-function(file,field,...) {
 
     out<-list()
 
-    # Read NetCDF file
-    ncin <- ncdf4::nc_open(file)
-    out[[1]]<-ncdf4::ncvar_get(ncin,field)
+    ncin <- ncdf4::nc_open(file) # Open NetCDF file
+    tmp<-ncdf4::ncvar_get(ncin,field) # Read field
+    tmp[tmp%==%navals]<-NA # Set NA values
+    out[[1]]<-tmp # Add field to list
+
     i=1
     for(addfield in list(...)) {
-      out[[i+1]]<-ncdf4::ncvar_get(ncin,addfield)
+      tmp<-ncdf4::ncvar_get(ncin,addfield) # Read additional field
+      tmp[tmp%==%navals]<-NA # Set NA values
+      out[[i+1]]<-tmp # Add field to list
       i=i+1
     }
 
