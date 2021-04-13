0ccc461070f257fafa7405864437f686456d40e5
atroman <roman.attinger@env.ethz.ch>
2021-04-13 20:17:04 +0200

    added functionality to only read subset of variable and modified approach to read lon / lat dimensions

--- R/rfile.R
+++ R/rfile.R
@@ -1,9 +1,17 @@
 #' Read NetCDF File
 #'
-#' @param file character string specifying the NetCDF file location
-#' @param field character string specifying the field to be read
-#' @param navals float specifying NA values
-#' @param ... arbitrary number of character strings specifying additional fields to be read
+#' @param file A character string specifying the NetCDF file location
+#' @param field A character string specifying the field to be read
+#' @param ... An arbitrary number of character strings specifying additional fields to be read
+#' @param navals A float specifying NA values
+#' @param lonmin A float specifying longitude to start reading (optional)
+#' @param lonmax A float specifying longitude to stop reading (optional)
+#' @param latmin A float specifying latitude to start reading (optional)
+#' @param latmax A float specifying latitude to stop reading (optional)
+#' @param zstart An integer specifying first level to read (default: 1)
+#' @param zcount An integer specifying number of levels to read (default: -1)
+#' @param tstart An integer specifying first time-steps to read (default: 1)
+#' @param tcount An integer specifying number of time-steps to read (default: -1)
 #'
 #' @return
 #' @export
@@ -12,7 +20,7 @@
 #' library(zeallot) # Need this to unpack data from rfile
 #' c(APVTURBT)%<-%rfile("/net/thermo/atmosdyn2/atroman/stingjet/20140211_12/tra/APV12_20140212_07.nc","APVTURBT")
 
-rfile <-function(file,field,...,navals=-999) {
+rfile <-function(file,field,...,navals=NA,lonmin=NA,lonmax=NA,latmin=NA,latmax=NA,zstart=1,zcount=-1,tstart=1,tcount=-1) {
 
   # Check if file exists
   if(!file.exists(file)) {
@@ -21,20 +29,76 @@ rfile <-function(file,field,...,navals=-999) {
 
   } else {
 
+    # Initialize output list
     out<-list()
 
-    ncin <- ncdf4::nc_open(file) # Open NetCDF file
-    tmp<-ncdf4::ncvar_get(ncin,field) # Read field
-    tmp[tmp%==%navals]<-NA # Set NA values
-    out[[1]]<-tmp # Add field to list
+    # Open NetCDF file
+    ncin <- ncdf4::nc_open(file)
 
-    i=1
-    for(addfield in list(...)) {
-      tmp<-ncdf4::ncvar_get(ncin,addfield) # Read additional field
-      tmp[tmp%==%navals]<-NA # Set NA values
-      out[[i+1]]<-tmp # Add field to list
-      i=i+1
+    # Try to read longitude and latitude from
+    longitudes <- tryCatch(
+      {
+        ncdf4::ncvar_get(ncin,"lon") # Get longitudes
+      },
+      error=function(cond) {
+        message("ERROR: Longitudes not found on file!")
+        message(cond)
+        return(NA)
+      })
+    latitudes <- tryCatch(
+      {
+        ncdf4::ncvar_get(ncin,"lat") # Get latitudes
+      },
+      error=function(cond) {
+        message("ERROR: Latitudes not found on file!")
+        message(cond)
+        return(NA)
+      })
+
+    # Check if subset should be read
+    if(!is.na(lonmin) & !is.na(lonmax) & !is.na(latmin) & !is.na(latmax)) {
+
+      # Define lon/lat start and lon/lat count
+      lonstart<-which(longitudes%==%lonmin)
+      lonend<-which(longitudes%==%lonmax)
+      latstart<-which(latitudes%==%latmin)
+      latend<-which(latitudes%==%latmax)
+
+      # Error handling
+      if(length(lonstart)==0) warning(paste("ERROR: Latitude ",lonmin," not a subset of longitudes!",sep=""))
+      if(length(lonend)==0) warning(paste("ERROR: Latitude ",lonmax," not a subset of longitudes!",sep=""))
+      if(length(latstart)==0) warning(paste("ERROR: Latitude ",latmin," not a subset of latitudes!",sep=""))
+      if(length(latend)==0) warning(paste("ERROR: Latitude ",latmax," not a subset of latitudes!",sep=""))
+
+      # Calculate number of requested grid-points
+      loncount<-lonend-lonstart
+      latcount<-latend-latstart
+
+      # Redefine longitude and latitude vectors
+      longitudes<-longitudes[lonstart:lonend]
+      latitudes<-latitudes[latstart:latend]
+    } else {
+      # Default definitions
+      lonstart=1;loncount=-1;latstart=1;latcount=-1
     }
+
+    # Save longitudes and latitudes to output list
+    out[[1]]<-longitudes
+    out[[2]]<-latitudes
+
+    # Read subset or complete field
+    tmp<-ncdf4::ncvar_get(ncin,field,start=c(lonstart,latstart,zstart,tstart),count=c(loncount,latcount,zcount,tcount))
+
+    if(!is.na(navals)) tmp[tmp%==%navals]<-NA # Set NA values if required
+    out[[3]]<-tmp # Add field to list
+
+    # i=4
+    # for(addfield in list(...)) {
+    #   tmp<-ncdf4::ncvar_get(ncin,addfield,start=c(lonstart,latstart,zstart,tstart),count=c(loncount,latcount,zcount,tcount)) # Read additional field
+    #   if(!is.na(navals)) tmp[tmp%==%navals]<-NA # Set NA values if required
+    #   out[[i]]<-tmp # Add field to list
+    #   i=i+1 # Increment additional field counter
+    # }
 
     ncdf4::nc_close(ncin)
 
