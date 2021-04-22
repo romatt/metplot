0e38279cb3a0f814ee5aff7333853bed0107b968
Roman Attinger <roman.attinger@env.ethz.ch>
2021-04-22 20:50:11 +0200

    Updated requal function to use both shorthand and function name

--- R/requal.R
+++ R/requal.R
@@ -4,13 +4,16 @@
 #' @param testval value to be tested
 #' @param epsilon float value indicating how large difference between values is allowed to be
 #'
+#' @rdname requal
+#'
 #' @return
 #' @export
 #'
 #' @examples
 #' c(0.01,0.02,0.011)%==%0.01 equals TRUE FALSE  TRUE
-
-"%==%" <- function(values,testval,epsilon=0.01) {
+#'
+#' requal(c(0.01,0.02,0.011),0.01)
+requal <- function(values,testval,epsilon=0.01) {
 
   out<-sapply(values, function(v) {
     if (v+epsilon>testval && v-epsilon<testval){
@@ -22,3 +25,10 @@
   return(out)
 
 }
+
+#' Shorthand to test rough equality
+#'
+#' @rdname requal
+#' @return
+#' @export
+`%==%`  <- function(values, testval) requal(values, testval)
